c494379233aa8a267b34fbb9f9b41b8bc870c62e
Mihail Borovkov <1mihails.b@gmail.com>
2023-05-21 19:55:58 +0300

    concurrent requests working with long block inside task

--- src/main/java/org/streaming/App.java
+++ src/main/java/org/streaming/App.java
@@ -8,9 +8,15 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
+import java.time.Duration;
 
 public class App {
     private static final int PORT = 5555;
+    private static Logger logger;
+
+    static {
+        logger = new Logger(App.class.getName());
+    }
 
     public static void main(String[] args) throws IOException {
         var serverSocketChannel = ServerSocketChannel.open();
@@ -30,15 +36,22 @@ public class App {
              * TODO: select() has overload with msTimeout param that will return after given timeout.
              * If sockets didn't get any data during that time - we can close them if we want.
              */
+            logger.info("Before select");
             serverSelector.select();
+            logger.info("After select");
             var selectedKeys = serverSelector.selectedKeys().iterator();
             while (selectedKeys.hasNext()) {
                 var key = selectedKeys.next();
+                logger.info("Key: " + key);
+
                 selectedKeys.remove();
                 if (!key.isValid()) {
+                    logger.info("Key invalid");
                 }
 
                 if (key.channel().equals(serverSocketChannel)) { // or `key.isAcceptable()`
+                    logger.info("New connection");
+
                     var socketChannel = serverSocketChannel.accept(); // new connection from client
                     socketChannel.configureBlocking(false);
                     socketChannel.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
@@ -47,41 +60,47 @@ public class App {
 //                    int interest = SelectionKey.OP_READ | SelectionKey.OP_WRITE;
                     int interest = SelectionKey.OP_READ;
                     socketChannel.register(serverSelector, interest);
-                } else if (key.isReadable()) {
-                    // incoming data on existing connection
+                } else if (key.isReadable()) { // incoming data on existing connection
                     key.cancel();
+                    // call the selector just to process the cancelled keys
+                    serverSelector.selectNow();
+
                     var chan = (SocketChannel) key.channel();
 
-                    processIncoming(chan, key);
+                    logger.info("Before processIncoming");
+                    processIncoming(chan);
+                    logger.info("After processIncoming");
                 }
             }
         }
     }
 
-    static void processIncoming(SocketChannel chan, SelectionKey key) {
+    static void processIncoming(SocketChannel chan) {
         Thread.ofVirtual().name("Request handler vThread").start(() -> {
             try {
-                final ByteBuffer readBuffer = ByteBuffer.allocate(65535);
+                final ByteBuffer readBuffer = ByteBuffer.allocate(65536);
                 while(true) {
-                    chan.configureBlocking(true);
+                    Thread.sleep(Duration.ofSeconds(2));
+//                    chan.configureBlocking(true);
                     int read = chan.read(readBuffer.clear());
-                    if (read == -1) {
+                    if (read == -1 || read == 0) {
                         chan.close();
                         break;
                     } else {
                         var bytes = new byte[read];
                         readBuffer.get(0, bytes);
-                        System.out.println(String.format(
+                        logger.info((String.format(
                                 "Data from client channel:\n%s:\n----------------------------------------\n%s",
                                 chan,
                                 new String(bytes)
-                        ));
+                        )));
 
-                        String html = "<html><body>Custom html response</body></html>";
+                        String html = "{\"data\": \"custom html response\"}";
                         final String CRLF = "\n\r"; // 13, 10
                         String response =
                                 "HTTP/2.0 200 OK" + CRLF
-                                        + "Content-Length: " + html.getBytes().length + CRLF
+                                        + "Access-Control-Allow-Origin: *" + CRLF
+                                        + "Content-Length: " + (html.getBytes().length + CRLF.getBytes().length) + CRLF
                                         + CRLF
                                         + html
                                         + CRLF + CRLF;
@@ -91,13 +110,19 @@ public class App {
                     }
                 }
             } catch (IOException e) {
-
-                System.out.println("IO exceptions in vThread");
-            } finally {
                 try {
-                    chan.close();
+                    if(chan.isOpen()) chan.close();
+                } catch (IOException ex) {
+                    logger.info("throw new RuntimeException(ex");
+                }
+            } catch (InterruptedException e) {
+                logger.info("InterruptedException e)");
+                throw new RuntimeException(e);
+            }  finally {
+                try {
+                    if(chan.isOpen()) chan.close();
                 } catch (IOException e) {
-                    System.out.println("Couldn't close chan in virtual thread");
+                    logger.info("Couldn't close chan in virtual thread");
                 }
             }
         });
---
+++ src/main/java/org/streaming/Logger.java
@@ -0,0 +1,16 @@
+package org.streaming;
+
+import java.util.logging.Level;
+
+public class Logger {
+    private java.util.logging.Logger logger;
+
+    public Logger(String className) {
+        logger = java.util.logging.Logger.getLogger(className);
+        
+    }
+
+    public void info(String logString) {
+        logger.log(Level.INFO, "\u001B[32m" + logString + "\u001B[32m");
+    }
+}
